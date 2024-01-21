894a78c818cf50826ea1670b8dc978cfa04e9172
rafaelmaturino <107051064+rafaelmaturino@users.noreply.github.com>
2024-01-21 09:08:44 -0300

    Create dados-atletas.js

---
+++ dados-atletas.js
@@ -0,0 +1,94 @@
+// aplicação capaz de receber informações de um atleta, bem como calcular parâmetros e exibi-los para o usuário.
+class Atleta{
+ constructor(nome, idade, peso, altura, notas){
+  this.nome = nome;
+  this.idade = idade;
+  this.peso = peso;
+  this.altura = altura;
+  this.notas = notas;
+ }
+
+calculaCategoria() {
+  if (this.idade >=9 && this.idade <=11){
+    return "Infantil";
+  } 
+  else if(this.idade >=12 && this.idade <=13){
+    return "Juvenil";
+  }
+  else if(this.idade >=14 && this.idade <=15){
+    return "Intermediario";
+  }
+  else if(this.idade >=16 && this.idade <=30){
+    return "Adulto";
+  }
+  else{
+    return "demais idades";
+    }
+  }
+
+calculaIMC(){
+  return this.peso / (this.altura*this.altura);
+}
+
+calculaMediaValida() {
+  
+  let notasValidas = this.notas.map(function(nota){
+    if(nota >=0 && nota <=10){
+        return nota;
+    }
+    else{
+      return "Notas não são validas";
+    }
+  })
+  notasValidas = notasValidas.sort().slice(1,4);
+  let soma = 0;
+  notasValidas.forEach(function(nota){
+    soma = soma + nota;
+  })
+  return soma / notasValidas.length;
+   
+}
+
+obtemNomeAtleta(){
+    return this.nome;
+  }
+
+obtemIdadeAtleta(){
+     return this.idade;
+  }
+
+obtemPesoAtleta(){
+    return this.peso;
+ }
+
+obtemAlturaAtleta(){
+  return this.altura;
+}
+
+obtemNotasAtleta(){
+    return this.notas;
+ }
+
+obtemCategoria(){
+    return this.calculaCategoria();
+ }
+
+obtemIMC(){
+    return this.calculaIMC();
+ }
+
+obtemMediaValida(){
+    return this.calculaMediaValida();
+ }
+
+}
+let atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+console.log("Nome:", atleta.obtemNomeAtleta());
+console.log("Idade:", atleta.obtemIdadeAtleta());
+console.log("Peso:", atleta.obtemPesoAtleta());
+console.log("Altura:", atleta.obtemAlturaAtleta());
+console.log("Notas:", atleta.obtemNotasAtleta());
+console.log("Categoria:", atleta.obtemCategoria());
+console.log("IMC:", atleta.obtemIMC());
+console.log("Média válida:", atleta.obtemMediaValida());
